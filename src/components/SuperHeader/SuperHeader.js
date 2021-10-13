307c26704a37d7b7d2b805c4e338834c6d3a1f26
tripplybrand <tripplybrand@gmail.com>
2021-10-13 10:08:28 -0400

    Finished exercise 1: Superheader.

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-import { COLORS } from '../../constants';
+import { COLORS } from '../../constants'
 
-import SearchInput from '../SearchInput';
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
+import SearchInput from '../SearchInput'
+import UnstyledButton from '../UnstyledButton'
+import Icon from '../Icon'
 
 const SuperHeader = () => {
   return (
@@ -13,24 +13,29 @@ const SuperHeader = () => {
       <MarketingMessage>
         Free shipping on domestic orders over $75!
       </MarketingMessage>
-      <SearchInput />
-      <HelpLink href="/help">Help</HelpLink>
-      <UnstyledButton>
-        <Icon id="shopping-bag" strokeWidth={1} />
-      </UnstyledButton>
+      <MenuWrapper>
+        <SearchInput />
+        <HelpLink href="/help">Help</HelpLink>
+        <UnstyledButton>
+          <Icon id="shopping-bag" strokeWidth={1} />
+        </UnstyledButton>
+      </MenuWrapper>
     </Wrapper>
-  );
-};
+  )
+}
 
 const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
-`;
+`
 
 const MarketingMessage = styled.span`
+  margin: 0.75rem auto 0.75rem 2rem;
   color: ${COLORS.white};
-`;
+`
 
 const HelpLink = styled.a`
   color: inherit;
@@ -40,6 +45,14 @@ const HelpLink = styled.a`
   &:not(:focus-visible) {
     outline: none;
   }
-`;
+`
 
-export default SuperHeader;
+const MenuWrapper = styled.div`
+  display: flex;
+  margin-right: 2rem;
+  gap: 1.5rem;
+  margin-top: 0.75rem;
+  margin-bottom: 0.75rem;
+`
+
+export default SuperHeader
