56288046aeb248045fc518b7d247aba3585855b0
tripplybrand <tripplybrand@gmail.com>
2021-10-15 16:08:20 -0400

    Exercise 4B: Final touches

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -25,25 +25,34 @@ const ShoeCard = ({
   // both on-sale and new-release, but in this case, `on-sale`
   // will triumph and be the variant used.
   // prettier-ignore
-  const variant = typeof salePrice === 'number'
+  const onSale = typeof salePrice === 'number'
+  const variant = onSale
     ? 'on-sale'
     : isNewShoe(releaseDate)
-      ? 'new-release'
-      : 'default'
+    ? 'new-release'
+    : 'default'
 
   return (
     <Link href={`/shoe/${slug}`}>
       <Wrapper>
         <ImageWrapper>
+          {variant === 'default' ? null : variant === 'new-release' ? (
+            <Flag variant={variant}>Just Released!</Flag>
+          ) : (
+            <Flag variant={variant}>Sale</Flag>
+          )}
           <Image alt="" src={imageSrc} />
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price onSale={onSale}>{formatPrice(price)}</Price>
         </Row>
         <Row>
           <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+          {typeof salePrice === 'number' ? (
+            <SalePrice>{formatPrice(salePrice)}</SalePrice>
+          ) : null}
         </Row>
       </Wrapper>
     </Link>
@@ -78,15 +87,33 @@ const Name = styled.h3`
   margin-right: auto;
 `
 
-const Price = styled.span``
+const Price = styled.span`
+  color: ${(props) => (!props.onSale ? COLORS.gray[900] : COLORS.gray[700])};
+  text-decoration: ${(props) => (!props.onSale ? 'none' : 'line-through')};
+`
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
+  margin-right: auto;
 `
 
 const SalePrice = styled.span`
   font-weight: ${WEIGHTS.medium};
   color: ${COLORS.primary};
+`
+
+const Flag = styled.div`
+  background-color: ${(props) =>
+    props.variant === 'new-release' ? COLORS.secondary : COLORS.primary};
+  width: max-content;
+  padding: 0.3rem;
+  border-radius: 2px;
+  font-weight: ${WEIGHTS.bold};
+  font-size: 0.875rem;
+  color: white;
+  position: absolute;
+  right: 0px;
+  bottom: 88.11%;
 `
 
 export default ShoeCard
